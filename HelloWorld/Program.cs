043ef803c672bffdca8d81822d0fa5d8221b1984
Jack Beck <5306625+thejackbeck@users.noreply.github.com>
2022-03-24 09:08:31 -0700

    Initial commit.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello World!!!");
+
+        for (int i = 0; i < 10; ++i)
+        {
+            System.Console.WriteLine(i + " Mississippi...");
+        }
+    }
+}
